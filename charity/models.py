09615a4135673575c29e1ec0c33d1b5cb308d343
AmirHmn <amir.hosein6979@gmail.com>
2021-09-05 17:37:39 +0430

    Add product model

--- charity/models.py
+++ charity/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid
+import os
 
-# Create your models here.
+
+def get_product_avatar_path(instance, filename):
+    ext = filename.split(".")[-1]
+    # create random file
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join("products/avatars", filename)
+
+
+class Products(models.Model):
+    STATUS_CHOICES = (
+        ('a', 'available'),
+        ('s', 'sold'),
+    )
+    name = models.CharField(max_length=200, verbose_name="نام محصول")
+    description = models.CharField(max_length=250, blank=True, null=True, verbose_name="توضیحات")
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name="وضعیت")
+    picture = models.ImageField(
+        upload_to=get_product_avatar_path,
+        blank=True,
+        null=True,
+        verbose_name="تصویر محصول"
+    )
