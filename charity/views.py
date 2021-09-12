c6c990a0ce1db1d2f666a6172768529c51866fca
AmirHmn <amir.hosein6979@gmail.com>
2021-09-12 16:48:21 +0430

    Add product APIs for Charity App

--- charity/views.py
+++ charity/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Products
+from .serializers import ProductsSerializer
+from persons.permissions import IsUser
 
-# Create your views here.
+
+class ProductsList(ListAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+
+# GET specific product details
+class ProductDetail(RetrieveAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+# create new user only by admin
+class CreateProduct(CreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+# update specific user details
+class UpdateUser(RetrieveUpdateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    permission_classes = (IsUser,)
+
