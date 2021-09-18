36570517ccff57e2ba40dcbc36867473eedd9692
AmirHmn <amir.hosein6979@gmail.com>
2021-09-18 23:18:41 +0430

    Add category field for products

--- charity/admin.py
+++ charity/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import Products, Category
+from mptt.admin import MPTTModelAdmin
 
-# Register your models here.
+admin.site.register(Products)
+admin.site.register(Category, MPTTModelAdmin)
--- charity/models.py
+++ charity/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 import os
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 def get_product_avatar_path(instance, filename):
@@ -24,3 +25,21 @@ class Products(models.Model):
         null=True,
         verbose_name="تصویر محصول"
     )
+    category = TreeForeignKey('Category', null=True, blank=True,on_delete=models.CASCADE)
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True,
+                            on_delete=models.CASCADE)
+    slug = models.SlugField()
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        unique_together = (('parent', 'slug'),)
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
